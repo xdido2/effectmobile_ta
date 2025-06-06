526c2620f82bf973e4cd13d7bbfdc82408777bb9
abdurashid <abdurashiddido@gmail.com>
2025-06-06 08:45:37 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/effectmobile_ta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.effectmobile_ta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EffectMobileTaApp extends StatelessWidget {
+  const EffectMobileTaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
